5c224808fc03a7fcf39298aadfcc174aa8fe8daa
marchello10 <martyna.gajauskaite@gmail.com>
2020-09-16 21:16:37 +0300

    Updates

---
+++ precache-manifest.dbb3cd51536723c896104dde0b97f8fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b53c0a44db832d783c59d55ea7f71271",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "99e6539ded778d5aaad2",
+    "url": "/monsters/static/css/main.10882ca5.chunk.css"
+  },
+  {
+    "revision": "78adac64eaa07ff35dea",
+    "url": "/monsters/static/js/2.356d906f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters/static/js/2.356d906f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99e6539ded778d5aaad2",
+    "url": "/monsters/static/js/main.dd107675.chunk.js"
+  },
+  {
+    "revision": "3dab8aa598718cb8e8b3",
+    "url": "/monsters/static/js/runtime-main.f48c3991.js"
+  }
+]);
